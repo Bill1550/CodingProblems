18dd7789207bf2badd91ad15ccc3cfd4c407f29d
BillH <bill.hart@mavenclinic.com>
2023-02-09 23:39:53 -0500

    Added coroutines to project.

--- app/src/main/java/com/loneoaktech/tests/codingproblems/MainActivity.kt
+++ app/src/main/java/com/loneoaktech/tests/codingproblems/MainActivity.kt
@@ -3,26 +3,48 @@ package com.loneoaktech.tests.codingproblems
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.loneoaktech.tests.codingproblems.ui.theme.CodingProblemsTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val scope = rememberCoroutineScope()
+            val counter = remember{ mutableStateOf(0) }
+            LaunchedEffect(true) {
+                while(isActive) {
+                    delay(1000)
+                    counter.value = counter.value+1
+                }
+            }
+
             CodingProblemsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Greeting("Android")
+
+
+                        Text("Counter=${counter.value}")
+
+                    }
+
+
+
                 }
             }
         }
